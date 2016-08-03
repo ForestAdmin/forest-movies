a9104c9dc4915eb0dca47ac19ebfe673c0b891b0
Arnaud Besnier <arnaudibesnier@gmail.com>
2016-08-03 19:51:44 -0400

    Activity Log - Users want to see when an action is triggered on the activity log

--- app/controllers/forest/actions_controller.rb
+++ app/controllers/forest/actions_controller.rb
@@ -1,14 +1,14 @@
-class Forest::ActionsController < ForestLiana::ApplicationController
+class Forest::ActionsController < ForestLiana::ActionsController
   def approve
     comment = Comment.find(params['data']['attributes']['ids'][0])
     msg = "Comment by #{comment.customer.firstname} (##{comment.id}) approved."
-    render json: { success: msg}, status: 200
+    render json: { success: msg }, status: 200
   end
 
   def disapprove
     comment = Comment.find(params['data']['attributes']['ids'][0])
     msg = "Comment by #{comment.customer.firstname} (##{comment.id})
       disapproved."
-    render json: { success: msg}, status: 200
+    render json: { success: msg }, status: 200
   end
 end
