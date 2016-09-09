98d3309eca613b4b39fc2daf8ca50297be5b40d3
Arnaud Besnier <arnaudibesnier@gmail.com>
2016-09-09 10:43:32 +0200

    Forest - Update Forest integration and make database name homogeneous in dev

--- config/initializers/forest_liana.rb
+++ config/initializers/forest_liana.rb
@@ -4,12 +4,11 @@ ForestLiana.auth_key = 'KCzlh119AQ8OuTHoRxg0UQ'
 ForestLiana.integrations = {
   stripe: {
     api_key: ENV['STRIPE_SECRET_KEY'],
-    user_collection: 'Customer',
-    user_field: 'stripe_id'
+    mapping: 'Customer.stripe_id'
   },
   intercom: {
     app_id: ENV['INTERCOM_APP_ID'],
     api_key: ENV['INTERCOM_API_KEY'],
-    user_collection: 'Customer'
+    mapping: 'Customer'
   }
 }
