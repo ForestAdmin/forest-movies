d57c903a461d2aaaf7777bed21d9ad15dc4be474
Sandro Munda <sandro@munda.me>
2016-10-20 13:45:55 -0700

    Add the fullname smart field

--- lib/forest_liana/collections/customer.rb
+++ lib/forest_liana/collections/customer.rb
@@ -3,7 +3,14 @@ class Forest::Customer
 
   collection :customers
 
-  field :fullname, type: 'String' do
+  search_fullname = lambda do |query, search|
+    first_name, last_name = search.split
+    query.where_values.first << " OR (firstname = '#{first_name}' AND lastname = '#{last_name}')"
+
+    query
+  end
+
+  field :fullname, search: search_fullname , type: 'String' do
     "#{object.firstname} #{object.lastname}"
   end
 end
